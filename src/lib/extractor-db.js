3f6140f598cbd12a2f65237d4fda7846e2ec608f
SLOSHYMX <devlnicanorl@gmail.com>
2026-01-09 16:36:15 -0600

    Fix

--- src/lib/extractor-db.js
+++ src/lib/extractor-db.js
@@ -1,5 +1,7 @@
 // Wrapper para query que siempre usa la DB del extractor
-import { query as dbQuery } from "@/lib/db";
+import { query as dbQuery, isExtractorDbConfigured } from "@/lib/db";
+
+export { isExtractorDbConfigured };
 
 export async function query(text, params) {
   return dbQuery(text, params, true); // Siempre usar DB extractor
